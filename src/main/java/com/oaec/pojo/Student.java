b7d91a1aa2edaf6a9f8fda47711f652e75c87b76
vitomaojiawang <943939338@qq.com>
2018-08-28 16:05:08 +0800

    添加学生类

---
+++ src/main/java/com/oaec/pojo/Student.java
@@ -0,0 +1,27 @@
+package com.oaec.pojo;
+
+public class Student {
+	private String stu_name;
+	private String stu_pwd;
+	private String stu_score;
+	public String getStu_name() {
+		return stu_name;
+	}
+	public void setStu_name(String stu_name) {
+		this.stu_name = stu_name;
+	}
+	public String getStu_pwd() {
+		return stu_pwd;
+	}
+	public void setStu_pwd(String stu_pwd) {
+		this.stu_pwd = stu_pwd;
+	}
+	public String getStu_score() {
+		return stu_score;
+	}
+	public void setStu_score(String stu_score) {
+		this.stu_score = stu_score;
+	}
+	
+	
+}
