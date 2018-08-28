b59f809f6108dc0ad5fc3d678c7e3fb2df05caa0
vitomaojiawang <943939338@qq.com>
2018-08-28 16:40:56 +0800

    修改书籍信息

---
+++ src/main/java/com/oaec/pojo/Book.java
@@ -0,0 +1,15 @@
+package com.oaec.pojo;
+
+public class Book{
+	private String bookName;
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	
+	
+}
