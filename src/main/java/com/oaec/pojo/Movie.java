c195d759b4964d7eb86e4fbe28738a0a71292f14
vitomaojiawang <943939338@qq.com>
2018-08-28 16:25:56 +0800

    添加电影信息

---
+++ src/main/java/com/oaec/pojo/Movie.java
@@ -0,0 +1,14 @@
+package com.oaec.pojo;
+
+public class Movie {
+	private int movie_id;
+
+	public int getMovie_id() {
+		return movie_id;
+	}
+
+	public void setMovie_id(int movie_id) {
+		this.movie_id = movie_id;
+	}
+	
+}
