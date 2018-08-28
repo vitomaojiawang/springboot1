7d7e626d994ee96a74a37d6f7fa4aae83a76736a
vitomaojiawang <943939338@qq.com>
2018-08-28 16:50:56 +0800

    修改movie

--- src/main/java/com/oaec/pojo/Movie.java
+++ src/main/java/com/oaec/pojo/Movie.java
@@ -2,6 +2,16 @@ package com.oaec.pojo;
 
 public class Movie {
 	private int movie_id;
+	private String movie_name;
+	
+
+	public String getMovie_name() {
+		return movie_name;
+	}
+
+	public void setMovie_name(String movie_name) {
+		this.movie_name = movie_name;
+	}
 
 	public int getMovie_id() {
 		return movie_id;
