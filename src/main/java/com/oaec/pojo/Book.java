c224b311fcd1daf54bd97ec3fcfe9eeb7444004f
vitojiawang <260190037@qq.com>
2018-08-31 16:35:10 +0800

    添加书籍类型字段

--- src/main/java/com/oaec/pojo/Book.java
+++ src/main/java/com/oaec/pojo/Book.java
@@ -2,6 +2,15 @@ package com.oaec.pojo;
 
 public class Book{
 	private String bookName;
+	private String bookType;
+
+	public String getBookType() {
+		return bookType;
+	}
+
+	public void setBookType(String bookType) {
+		this.bookType = bookType;
+	}
 
 	public String getBookName() {
 		return bookName;
